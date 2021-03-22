03f5b918615e4a0a40c6fff834f91c67a268e1e6
Nathan Schrader <nathanschrader@icloud.com>
2021-03-22 14:37:30 -0700

    add directory menu with 4 div elements

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,3 +1,34 @@
 import React from 'react';
 
-const HomePage = ()
+const HomePage = () => (
+  <div className='homepage'>
+    <div className='directory-menu'>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>COMMUNITY</h1>
+          <span className='subtitle'>CONNECT NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>NUTRITION</h1>
+          <span className='subtitle'>SHOP NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>WORKOUTS</h1>
+          <span className='subtitle'>MOVE NOW</span>
+        </div>
+      </div>
+      <div className='menu-item'>
+        <div className='content'>
+          <h1 className='title'>TRAVEL</h1>
+          <span className='subtitle'>FOLLOW NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
