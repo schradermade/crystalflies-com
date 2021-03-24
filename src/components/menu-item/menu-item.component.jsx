0e8c1e3742cd46c1aa1305e9fbb35c975cb821eb
Nathan Schrader <nathanschrader@icloud.com>
2021-03-23 18:22:20 -0700

    separate .content CSS action from .menu-item action

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,12 @@ import './menu-item.styles.scss';
 
 // destructure title, subititle, etc off props
 const MenuItem = ({title, subtitle, imageUrl, size}) => (
-  <div 
-    style={{
-      backgroundImage: `url(${imageUrl})`
-    }}
+  <div
     className={`${size} menu-item`}
   >
+    <div className='background-image' style={{
+      backgroundImage: `url(${imageUrl})`
+    }} />
     <div className='content'>
       <h1 className='title'>{title.toUpperCase()}</h1>
       <span className='subtitle'>{subtitle.toUpperCase()}</span>
